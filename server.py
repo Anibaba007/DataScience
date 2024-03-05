c53632316e5bb80b948c07b93c5e5e0d88f7c459
Anibaba007 <anibabaabdulkadri@gmail.com>
2024-03-05 20:50:22 +0100

    python code

---
+++ server.py
@@ -0,0 +1,121 @@
+# name ='quadri'
+# age =31
+# print(age)
+# mycurrentage = 31
+# nextYear = 1
+# myagenextYear = mycurrentage + nextYear
+# print(myagenextYear) #
+
+# #string = anaything covered quotation mark "name"
+# #integer = whole number 
+# #float = carry a decimal
+# #Boolean = True or false
+
+# numberone  = 5
+# numbertwo = 10
+# print(numberone + numbertwo)
+
+# soundone ="woof"
+# soundtwo = "wood"
+# print(soundone + soundtwo)
+
+# print(type(soundone)) # to know the type of data type
+
+# # Excercise 
+# #Part 1
+#item_price = 19.99
+#item_quantity = 3
+#total_cost = (item_price*item_quantity)
+#print(total_cost)
+
+# #Part 2
+#store_name ="GPT Fashions"
+#new_receipt_message = 'You bought' + " " + str(item_quantity) + "from" + store_name + 'for a total of' + " " + str(total_cost)
+#print(new_receipt_message)
+
+#name = input('whats your name')
+#age = ('whats you age')
+#print(name)
+#print(age)
+
+#numbers = 3,5,6
+#print(sum(numbers))
+
+#part 3
+#average_cost = total_cost/item_quantity
+#print(average_cost)
+#average_cost_string = str(average_cost) + " is the average cost per unit"
+#print(average_cost_string)
+
+#classicNumb = sum((5,6))
+#print(classicNumb)
+
+#def introduceYourself(name, age):
+    #return "my name" + name + "and I am" + str(age)
+
+#introduction = introduceYourself('Quadri',31)
+#print(introduction)
+
+#def metreConverter(cm):
+#    return cm/100
+#resultinmeters = metreConverter(1000)
+#print(resultinmeters)
+
+#userAge = int(input("whats your age"))
+#def daysAlive(ageInYears):
+#    return ageInYears*365
+#numberofDaysAlive = daysAlive(userAge)
+#print(numberofDaysAlive) # user has been alive for about 11315 days
+
+# Exeecise 1
+# currency conversion
+#def currencyConverter(euro):
+ #   return euro * 4.2
+#resultInEuro = currencyConverter(50) # function name and argument
+#print(resultInEuro)
+
+#def convertCurrency():
+#    userAmount = int(input("how much do you want to convert"))
+#    return userAmount * 4.2
+#print(convertCurrency())
+
+# Excercise 2
+#def imperialConverter(distance, temperature):
+#    Fareinheit = (temperature * 1.8) + 32 # converrt celcius to Fareinheit
+#    miles = (distance * 0.62137119) # convert Km to miles
+#    return "I ran " + str(miles) + "in a heat of " + str(Fareinheit) + " F" # return the result
+#print(imperialConverter(42,21)) # call the function and pass the argument
+
+# Excercise 2
+#def traffic_light_action (color):
+#    if color == "green":
+#        return "go"
+#    elif color == "yellow":
+#        return "Slow Down"
+#    elif color == "red":
+#        return "Stop"
+#    else:
+#        return "Invalid color"
+#print(traffic_light_action("green"))
+
+# Exercise 3
+#def Footwear_decision (activity):
+#    if activity == "hiking":
+#        return "Hiking boots"
+#    elif activity == "swimming":
+#        return "Swim fins or water shoes"
+#    elif activity == "dancing":
+#        return "Dance shoes"
+#    elif activity == "ofice work":
+#        return "Formal shoes"
+#    else:
+#        return "Sneakers of casual shoes"
+#print(Footwear_decision ("hiking"))
+
+
+
+
+
+
+
+
